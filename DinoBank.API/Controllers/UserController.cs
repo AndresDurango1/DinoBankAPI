3937a013933e2e9477b7bc4832fabec245152cdc
Andres Durango <andres.durango1@outlook.com>
2025-05-06 18:35:16 -0500

    Primer commit API DinoBank

---
+++ DinoBank.API/Controllers/UserController.cs
@@ -0,0 +1,95 @@
+﻿using DinoBank.Domain.User;
+using DinoBank.Persistence.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DinoBank.API.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IDatabaseService _databaseService;
+        public UserController(IDatabaseService databaseService)
+        {
+            _databaseService = databaseService; //Se hace la inyeccion de dependencias en el método contructor
+        }
+        //GET - Obtener los usuarios del JSON
+        [HttpGet("get-all")]
+        public IActionResult GetAll()
+        {
+            //Get All
+            var data = _databaseService.GetAll();
+            if (data != null && data.Count == 0)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, "No hay datos");
+            }
+            return StatusCode(StatusCodes.Status200OK, data);
+        }
+        //GET BY ID - Obtener un usurio por ID del JSON
+        [HttpGet("get-by-id/{id}")]
+        public IActionResult GetById(int Id)
+        {
+            //Get By Id
+            var data = _databaseService.GetAll();
+            if (data == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, "No hay datos");
+            }
+            var user = data.FirstOrDefault(x => x.Id == Id);
+            return StatusCode(StatusCodes.Status200OK, user);
+        }
+        //POST - Crear un nuevo usuario en el JSON
+        [HttpPost("create")]
+        public IActionResult Create([FromBody] UserEntity user)
+        {
+            //Create
+            if (string.IsNullOrEmpty(user.UserName) ||
+                string.IsNullOrEmpty(user.Password) ||
+                string.IsNullOrEmpty(user.Type))
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "Faltan datos de usuario");
+            }
+            var data = _databaseService.Create(user);
+            if (!data)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error al crear el usuario");
+            }
+            return StatusCode(StatusCodes.Status200OK, data);
+        }
+        //PUT - Actualizar un usuario en el JSON
+        [HttpPut("update")]
+        public IActionResult Update([FromBody] UserEntity user)
+        {
+            //Update
+            if (user.Id == 0 ||
+                string.IsNullOrEmpty(user.UserName) ||
+                string.IsNullOrEmpty(user.Password) ||
+                string.IsNullOrEmpty(user.Type))
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "Faltan datos de usuario");
+            }
+            var data = _databaseService.Update(user);
+            if (!data)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error al actualizar el usuario");
+            }
+            return StatusCode(StatusCodes.Status200OK, data);
+        }
+        //DELETE - Eliminar un usuario en el JSON
+        [HttpDelete("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            //Delete
+            if (id == 0)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "Id no válido");
+            }
+            var data = _databaseService.Delete(id);
+            if (!data)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error al actualizar el usuario");
+            }
+            return StatusCode(StatusCodes.Status200OK, data);
+        }
+    }
+}
---
+++ DinoBank.Persistence/Database/DatabaseService.cs
@@ -0,0 +1,83 @@
+﻿using DinoBank.Domain.User;
+using Newtonsoft.Json;
+
+namespace DinoBank.Persistence.Database
+{
+    public class DatabaseService : IDatabaseService
+    {
+        private static string route = Path.Combine(Path.GetDirectoryName(Environment.CurrentDirectory), "DinoBank.Persistence", "Files");
+
+        //Metodo para obtener toda la informacipon de los usuarios del JSON
+        public List<UserEntity> GetAll()
+        {
+            var file = Path.Combine(route, "User.JSON");
+            using (StreamReader r = new StreamReader(file))
+            {
+                var json = r.ReadToEnd();
+                return JsonConvert.DeserializeObject<List<UserEntity>>(json) ?? new List<UserEntity>();
+            }
+        }
+        //Metodo para crear un nuevo usuario en el JSON
+        public bool Create(UserEntity user)
+        {
+            var file = Path.Combine(route, "User.JSON");
+            using (StreamReader r = new StreamReader(file))
+            {
+                var json = r.ReadToEnd();
+                r.Close();
+                var listUser = JsonConvert.DeserializeObject<List<UserEntity>>(json) ?? new List<UserEntity>();
+                user.Id = listUser.Count + 1;
+                listUser.Add(user);
+                json = JsonConvert.SerializeObject(listUser, Formatting.Indented);
+                File.WriteAllText(file, json);
+                return true;
+            }
+        }
+        //Metodo para actualizar un usuario en el JSON
+        public bool Update(UserEntity user)
+        {
+            var file = Path.Combine(route, "User.JSON");
+            using (StreamReader r = new StreamReader(file))
+            {
+                var json = r.ReadToEnd();
+                r.Close();
+                var listUser = JsonConvert.DeserializeObject<List<UserEntity>>(json);
+                if (listUser == null || listUser.Count == 0)
+                {
+                    return false;
+                }
+                var index = listUser.FindIndex(x => x.Id == user.Id);
+                if (index != -1)
+                {
+                    listUser[index] = user;
+                    json = JsonConvert.SerializeObject(listUser, Formatting.Indented);
+                    File.WriteAllText(file, json);
+                    return true;
+                }
+                return false;
+            }
+        }
+        //Metodo para eliminar un usuario del JSON
+        public bool Delete(int id)
+        {
+            var file = Path.Combine(route, "User.JSON");
+            using (StreamReader r = new StreamReader(file))
+            {
+                var json = r.ReadToEnd();
+                r.Close();
+                var listUser = JsonConvert.DeserializeObject<List<UserEntity>>(json);
+                if (listUser == null || listUser.Count == 0)
+                {
+                    return false;
+                }
+                bool remove = listUser.RemoveAll(x => x.Id == id) > 0;
+                if (remove)
+                {
+                    json = JsonConvert.SerializeObject(listUser, Formatting.Indented);
+                    File.WriteAllText(file, json);
+                }
+                return remove;
+            }
+        }
+    }
+}
